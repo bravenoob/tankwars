7b6b43db572d9e94dc39610f60ba8aac53bb0120
bravenoob <12645990+bravenoob@users.noreply.github.com>
2022-04-10 22:09:15 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,15 @@
-import os
-import random
-import requests
-import numpy as np
 import pandas as pd
+import requests
 import streamlit as st
-import seaborn as sns
-from replit.database import Database
 
+st.set_page_config(
+    page_title="Tankwars Statistic Tool",
+    page_icon=None,
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+    
 # main body
 st.header("Tankwars Statistic Tool | #tankwars_stats")
 
@@ -40,37 +43,6 @@ Current pair liquidity on Terraswap, $ASTRO token to liquidity ratio, and curren
 """
 )
 
-# sidebar
-st.sidebar.markdown(
-    f"""
-    # Assumptions
-    """
-)
-
-# astro price prediction
-astro_price = st.sidebar.number_input(
-    "$ASTRO Price", min_value=0.01, value=2.5, help="Price of $ASTRO"
-)
-
-# requests headers
-headers = {
-    "authority": "api.coinhall.org",
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36",
-    "accept": "*/*",
-    "sec-gpc": "1",
-    "origin": "https://coinhall.org",
-    "sec-fetch-site": "same-site",
-    "sec-fetch-mode": "cors",
-    "sec-fetch-dest": "empty",
-    "referer": "https://coinhall.org/",
-    "accept-language": "en-US,en;q=0.9",
-}
-
-
-# replit database
-#db = Database(
-#    "https://kv.replit.com/v0/eyJhbGciOiJIUzUxMiIsImlzcyI6ImNvbm1hbiIsImtpZCI6InByb2Q6MSIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJjb25tYW4iLCJleHAiOjE2Mzk5NzQzNDMsImlhdCI6MTYzOTg2Mjc0MywiZGF0YWJhc2VfaWQiOiI2MTA3MDMxZC00ZTY0LTQzNTAtOWM5NS00NmQ0MWNkNDJkYmUifQ.jlsw5c7Lg4afcejjTNBcqYRvo6-fMHWAS9ICWjFj9nUvK-jsxM5IbFwayZ3e6dAQSn2m2pNVKV6C7VRsZ3ZIBQ"
-#)
 
 
 # disclaimer
